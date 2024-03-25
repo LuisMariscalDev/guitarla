7c4c2488d317998f3033968f7fdb8854ebd086e3
Luis Mariscal <luisferdev16@gmail.com>
2024-03-25 17:56:19 -0600

    project finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,19 @@ import { db } from "./data/db.js";
 
 function App() {
 
-  const [data, setData] = useState(db);
-  const [cart, setCart] =useState([]);
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
+  const [data] = useState(db);
+  const [cart, setCart] =useState(initialCart);
   const MAX_ITEMS = 5;
   const MIN_ITEMS = 1;
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
 
   function addToCart(item) {
 
